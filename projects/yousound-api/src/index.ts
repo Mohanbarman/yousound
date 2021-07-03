6207465ead61a04aaaed043704876e4f7100c363
mohanbarman <mohanbarman9711@gmail.com>
2021-07-03 23:03:17 +0530

    Wiring up

---
+++ projects/yousound-api/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+import YoutubeApi from "./services/YoutubeApi";
+
+import * as env from "./config/env";
+import * as AuthControllers from "./controllers/AuthControllers";
+import * as OauthControllers from "./controllers/OauthControllers";
+import * as UserControllers from "./controllers/UserControllers";
+
+const app = express();
+
+// shared objects
+export const youtubeApi = new YoutubeApi({
+  clientId: env.CLIENT_ID,
+  clientSecret: env.CLIENT_SECRET,
+  redirectUri: env.REDIRECT_URL,
+});
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.get("/video/:videoId", (req, res) => {
+  const videoId = req.params.videoId;
+  res.send(`<h1>${videoId}</h1>`);
+});
+
+app.get("/login", AuthControllers.loginController);
+app.get("/oauth/google", OauthControllers.googleAuthorizationController);
+app.get("/me", UserControllers.meController);
+
+app.listen(env.PORT, () => console.log(`Alive on port ${env.PORT} 🚀`));
