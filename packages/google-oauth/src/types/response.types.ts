47ed4097795ad9d3d5ae87c8afa3825a08d8c993
mohanbarman <mohanbarman9711@gmail.com>
2021-07-04 23:12:59 +0530

    Adding oauth config type

---
+++ packages/google-oauth/src/types/response.types.ts
@@ -0,0 +1,24 @@
+export interface IAccessTokenResponse {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
+  id_token: string;
+}
+
+export interface IUserInfoResponse {
+  data: IUserInfoResponseData;
+  error: null | string;
+}
+
+export interface IUserInfoResponseData {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  locale: string;
+}
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./return.types";
+export * from "./oauth.types";
