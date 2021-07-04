e9d81b4703c3496738c99024412db1e5723fd518
mohanbarman <mohanbarman9711@gmail.com>
2021-07-04 23:10:15 +0530

    Adding playlist response types

---
+++ packages/youtube-api/src/types/playlists.types.ts
@@ -0,0 +1,59 @@
+export interface IPlaylistsOptions {
+  part: Array<"contentDetails" | "id" | "localizations" | "player" | "snippet" | "status">;
+  channelId?: string;
+  id?: string;
+  mine?: boolean;
+}
+
+export interface IPlaylistsResponse {
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: IPlaylistItem[];
+}
+
+export interface IPlaylistItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: IPlaylistSnippet;
+  contentDetails: IPlaylistContentDetails;
+}
+
+export interface IPlaylistContentDetails {
+  itemCount: number;
+}
+
+export interface IPlaylistSnippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  localized: IPlaylistLocalized;
+}
+
+export interface IPlaylistLocalized {
+  title: string;
+  description: string;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Default;
+  high: Default;
+  standard: Default;
+  maxres: Default;
+}
+
+export interface Default {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
