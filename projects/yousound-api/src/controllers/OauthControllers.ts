c11336dc9b04a133c4f3eaf20b3a44eb6c003b79
mohanbarman <mohanbarman9711@gmail.com>
2021-07-04 23:08:03 +0530

    Implementing jwt in redirect uri callback

--- projects/yousound-api/src/controllers/OauthControllers.ts
+++ projects/yousound-api/src/controllers/OauthControllers.ts
@@ -1,20 +1,27 @@
 import { Request, Response } from "express";
-import { youtubeApi } from "..";
+import jwt from "jsonwebtoken";
+
+import { googleOauth } from "..";
+import { JWT_ACCESS_TOKEN } from "../config/env";
 
 export const googleAuthorizationController = async (req: Request, res: Response): Promise<void> => {
   const authCode = req.query.code;
 
+  // code param not present
   if (typeof authCode !== "string") {
     res.json({ error: "code is required" });
     return;
   }
 
-  const authToken = await youtubeApi.getToken(authCode);
-  youtubeApi.setToken(authToken);
+  // getting oauth token object from google oauth server
+  const oauthToken = await googleOauth.getToken(authCode);
 
-  const accessToken = authToken.tokens.access_token;
+  // creating jwt signed key containing refresh and accesstokens
+  const bearerToken = jwt.sign(oauthToken.data, JWT_ACCESS_TOKEN);
 
-  res.cookie("Authorization", accessToken);
+  // setting Authorization cookie with bearer token
+  res.cookie("Authorization", `Bearer ${bearerToken}`);
 
+  // redirecting (optional)
   res.redirect("/me");
 };
