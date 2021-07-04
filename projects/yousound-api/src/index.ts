7ed4ed42d108b3a6cdfb5213773a6de94cd4adfe
mohanbarman <mohanbarman9711@gmail.com>
2021-07-04 23:09:35 +0530

    Using google and youtube api from package, adding more endpoints

--- projects/yousound-api/src/index.ts
+++ projects/yousound-api/src/index.ts
@@ -1,35 +1,31 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
-import YoutubeApi from "./services/YoutubeApi";
+import { GoogleOauth } from "@packages/google-oauth";
+import { YoutubeApi } from "@packages/youtube-api";
 
 import * as env from "./config/env";
+import { googleOauthConfig } from "./config";
+import { authenticateUser } from "./middlewares/authenticationMiddleware";
+
 import * as AuthControllers from "./controllers/AuthControllers";
 import * as OauthControllers from "./controllers/OauthControllers";
 import * as UserControllers from "./controllers/UserControllers";
-import { authorizationMiddleware } from "./middlewares/authorizationMiddleware";
+import * as VideoController from "./controllers/VideoControllers";
 
 const app = express();
 
 // shared objects
-export const youtubeApi = new YoutubeApi({
-  clientId: env.CLIENT_ID,
-  clientSecret: env.CLIENT_SECRET,
-  redirectUri: env.REDIRECT_URL,
-});
+export const googleOauth = new GoogleOauth(googleOauthConfig);
+export const youtubeApi = new YoutubeApi(googleOauthConfig);
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.get("/video/:videoId", (req, res) => {
-  const videoId = req.params.videoId;
-  res.send(`<h1>${videoId}</h1>`);
-});
-
 app.get("/login", AuthControllers.loginController);
 app.get("/oauth/google", OauthControllers.googleAuthorizationController);
-app.get("/me", authorizationMiddleware, UserControllers.meController);
+app.get("/me", authenticateUser, UserControllers.meController);
+app.get("/playlists", authenticateUser, VideoController.getMyPlaylists);
 
 app.listen(env.PORT, () => console.log(`Alive on port ${env.PORT} 🚀`));
