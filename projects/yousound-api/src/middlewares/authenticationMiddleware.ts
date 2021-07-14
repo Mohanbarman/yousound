f9a91829e76c8f3d359303773f5c2207f479c1d6
mohanbarman <mohanbarman9711@gmail.com>
2021-07-14 20:34:27 +0530

    Finishing up authenticationMiddleware

--- projects/yousound-api/src/middlewares/authenticationMiddleware.ts
+++ projects/yousound-api/src/middlewares/authenticationMiddleware.ts
@@ -1,26 +1,69 @@
-import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import { IAccessTokenResponse } from "@packages/google-oauth";
+import { NextFunction, Response } from "express";
 
-import { JWT_ACCESS_TOKEN } from "../config/env";
+import { TRequest } from "../types/context.types";
+import { userModel } from "../models/UserModel";
+import { responseErrors } from "../utils";
+import { googleOauth } from "..";
 
+/**
+ * Middleware for authenticating user
+ * Responsible for:
+ * + Validating user's session
+ * + Refreshing access token of the user
+ * + Sets the user document object in request
+ */
 export const authenticateUser = async (
-  req: Request & { user: IAccessTokenResponse },
+  req: TRequest,
   res: Response,
   next: NextFunction
-): Promise<void> => {
-  const accessToken: string = req.cookies["Authorization"];
+): Promise<any> => {
+  const userId = req.session.userID;
 
-  if (!accessToken) {
-    res.sendStatus(401);
-    return;
+  /**
+   * userID in session is not present
+   */
+  if (!userId) {
+    return res.json(responseErrors.UNAUTHENTICATED_USER);
   }
 
-  const bearerToken = accessToken.split(" ")[1];
+  /**
+   * Getting user by userId
+   */
+  const user = await userModel.findById(userId);
 
-  jwt.verify(bearerToken, JWT_ACCESS_TOKEN, (err, user: IAccessTokenResponse) => {
-    if (err) return res.sendStatus(401);
-    req.user = user;
-    next();
-  });
+  /**
+   * There is no user in database with this userId
+   */
+  if (!user) {
+    return res.json(responseErrors.AUTH_USER_NOT_FOUND);
+  }
+
+  /**
+   * Checking if access token is expired
+   */
+  if (user.accessTokenExp < new Date()) {
+    // getting new access token from google oauth api
+    const { data, error } = await googleOauth.refreshAccessToken(
+      user.refreshToken
+    );
+
+    /**
+     * If failes to get new access token then clear the
+     * sesssion to get new refresh token when user authenticates again
+     */
+    if (error) {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      req.session.destroy(() => {}); // Clearing the user's session
+      return res.json(responseErrors.OAUTH_ACCESS_TOKEN);
+    }
+
+    // updating new access token in database
+    user.accessToken = data.access_token;
+    user.accessTokenExp = new Date(Date.now() + data.expires_in * 1000);
+    await user.save();
+  }
+
+  // setting user in req object to be used by next middleware
+  req.user = user;
+  next();
 };
