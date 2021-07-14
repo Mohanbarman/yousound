f4f616a2bebb4b94392e1130c7e13d01afc31dd9
mohanbarman <mohanbarman9711@gmail.com>
2021-07-14 20:33:55 +0530

    Finishing up controller for oauth

--- projects/yousound-api/src/controllers/OauthControllers.ts
+++ projects/yousound-api/src/controllers/OauthControllers.ts
@@ -1,10 +1,13 @@
-import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { Response } from "express";
 
+import { userModel } from "../models/UserModel";
+import { TRequest } from "../types/context.types";
 import { googleOauth } from "..";
-import { JWT_ACCESS_TOKEN } from "../config/env";
 
-export const googleAuthorizationController = async (req: Request, res: Response): Promise<void> => {
+export const googleAuthorizationController = async (
+  req: TRequest,
+  res: Response
+): Promise<void> => {
   const authCode = req.query.code;
 
   // code param not present
@@ -16,12 +19,36 @@ export const googleAuthorizationController = async (req: Request, res: Response)
   // getting oauth token object from google oauth server
   const oauthToken = await googleOauth.getToken(authCode);
 
-  // creating jwt signed key containing refresh and accesstokens
-  const bearerToken = jwt.sign(oauthToken.data, JWT_ACCESS_TOKEN);
+  // getting general user info
+  const googleUser = await googleOauth.getUserInfo(
+    oauthToken.data.id_token,
+    oauthToken.data.access_token
+  );
 
-  // setting Authorization cookie with bearer token
-  res.cookie("Authorization", `Bearer ${bearerToken}`);
+  // checking if user already exists
+  const dbUser = await userModel.findOne({ email: googleUser.data.email });
 
-  // redirecting (optional)
+  // Updating oauth tokens if user already exists
+  if (dbUser) {
+    dbUser.update({
+      accessToken: oauthToken.data.access_token,
+      refreshToken: oauthToken.data.refresh_token,
+      accessTokenExp: new Date(Date.now() + oauthToken.data.expires_in * 1000),
+    });
+    req.session.userID = dbUser.id;
+    return res.redirect("/me");
+  }
+
+  // creating new user
+  const userDoc = await userModel.create({
+    name: googleUser.data.name,
+    email: googleUser.data.email,
+    accessToken: oauthToken.data.access_token,
+    refreshToken: oauthToken.data.refresh_token,
+    accessTokenExp: new Date(Date.now() + oauthToken.data.expires_in * 1000),
+  });
+
+  req.session.userID = userDoc.id;
+
   res.redirect("/me");
 };
