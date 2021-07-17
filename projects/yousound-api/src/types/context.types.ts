96ca4ef9c7278d36df7c2056045bdc61ab87ee39
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 01:01:41 +0530

    Swithing to typeorm

--- projects/yousound-api/src/types/context.types.ts
+++ projects/yousound-api/src/types/context.types.ts
@@ -1,11 +1,10 @@
 import { Request } from "express";
 import session from "express-session";
-import { Document } from "mongoose";
-import { IUser } from "./user.types";
+import { UserEntity } from "../entities";
 
 type TSessionUser = { userID: string };
 
 export type TRequest = Request & {
   session: session.Session & Partial<session.SessionData> & TSessionUser;
-  user: IUser & Document<any, any, IUser>;
+  user?: UserEntity | null;
 };
