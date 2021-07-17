1ce5c6cc5f352ac76cf735452a8f3fdb4fc18e48
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 01:03:51 +0530

    adding validateParam validator

---
+++ projects/yousound-api/src/validators/validateParam.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import * as yup from "yup";
+import { Request, NextFunction, Response } from "express";
+import { buildResponse } from "../utils/buildResponse";
+
+/**
+ * Validate a url param
+ * Example /users/:id
+ * @param name validator name, format : validators/fieldName
+ */
+export const validateParam =
+  (name: string, paramName: string) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const [type, field] = name.split("/");
+    const param = req.params[field];
+
+    const fields = await import(`./${type}/fields.ts`);
+    const fieldSchema = fields[field];
+
+    const yupSchema = yup.object().shape({
+      [paramName]: fieldSchema,
+    });
+
+    try {
+      await yupSchema.validate({ [paramName]: param });
+      next();
+    } catch (e) {
+      res.json(
+        buildResponse({
+          error: { code: "VALIDATION_FAILED", validationErrors: e },
+        })
+      );
+    }
+  };
