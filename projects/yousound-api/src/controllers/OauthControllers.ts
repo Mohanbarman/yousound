b3e90cb78c9402cc1d00dbd03595ac0c75eeceaa
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 00:57:00 +0530

    Switching to typeorm from mongoose

--- projects/yousound-api/src/controllers/OauthControllers.ts
+++ projects/yousound-api/src/controllers/OauthControllers.ts
@@ -1,13 +1,15 @@
 import { Response } from "express";
 
-import { userModel } from "../models/UserModel";
 import { TRequest } from "../types/context.types";
-import { googleOauth } from "..";
+import { UserEntity } from "../entities";
+import { googleOauth } from "../app";
+import { userHelpers } from "../utils/userHelpers";
+import { responseErrors } from "../utils";
 
 export const googleAuthorizationController = async (
   req: TRequest,
   res: Response
-): Promise<void> => {
+): Promise<any> => {
   const authCode = req.query.code;
 
   // code param not present
@@ -19,6 +21,11 @@ export const googleAuthorizationController = async (
   // getting oauth token object from google oauth server
   const oauthToken = await googleOauth.getToken(authCode);
 
+  // failed to get oauth access token
+  if (oauthToken.error) {
+    return res.json(responseErrors.OAUTH_CODE_FAILED);
+  }
+
   // getting general user info
   const googleUser = await googleOauth.getUserInfo(
     oauthToken.data.id_token,
@@ -26,29 +33,34 @@ export const googleAuthorizationController = async (
   );
 
   // checking if user already exists
-  const dbUser = await userModel.findOne({ email: googleUser.data.email });
+  const dbUser = await userHelpers.getByEmail(googleUser.data.email);
 
   // Updating oauth tokens if user already exists
   if (dbUser) {
-    dbUser.update({
-      accessToken: oauthToken.data.access_token,
-      refreshToken: oauthToken.data.refresh_token,
-      accessTokenExp: new Date(Date.now() + oauthToken.data.expires_in * 1000),
-    });
-    req.session.userID = dbUser.id;
+    dbUser.accessToken = oauthToken.data.access_token;
+    dbUser.refreshToken = oauthToken.data.refresh_token;
+    dbUser.accessTokenExp = new Date(
+      Date.now() + oauthToken.data.expires_in * 1000
+    );
+    req.session.userID = dbUser.id.toString();
     return res.redirect("/me");
   }
 
   // creating new user
-  const userDoc = await userModel.create({
-    name: googleUser.data.name,
-    email: googleUser.data.email,
-    accessToken: oauthToken.data.access_token,
-    refreshToken: oauthToken.data.refresh_token,
-    accessTokenExp: new Date(Date.now() + oauthToken.data.expires_in * 1000),
-  });
+  const newUser = new UserEntity();
 
-  req.session.userID = userDoc.id;
+  newUser.name = googleUser.data.name;
+  newUser.email = googleUser.data.email;
+  newUser.accessToken = oauthToken.data.access_token;
+  newUser.refreshToken = oauthToken.data.refresh_token;
+  newUser.accessTokenExp = new Date(
+    Date.now() + oauthToken.data.expires_in * 1000
+  );
+  newUser.profilePicture = googleUser.data.picture;
+
+  await userHelpers.save(newUser);
+
+  req.session.userID = newUser.id.toString();
 
   res.redirect("/me");
 };
