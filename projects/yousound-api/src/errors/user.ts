7ca2c6026270f4ec8d4a27ea878aa6a08000ce3d
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 00:59:06 +0530

    Adding youtube errors

---
+++ projects/yousound-api/src/errors/user.ts
@@ -0,0 +1,27 @@
+import { buildResponse } from "../utils";
+
+export const userErrors = {
+  unauthenticated: buildResponse({
+    success: false,
+    error: {
+      code: "USER_UNAUTHENTICATED",
+      description: "User is not authenticated",
+    },
+  }),
+
+  newAccessToken: buildResponse({
+    success: false,
+    error: {
+      code: "OAUTH_ACCESS_TOKEN",
+      description: "Failed to fetch new access token",
+    },
+  }),
+
+  codeFailed: buildResponse({
+    success: false,
+    error: {
+      code: "OAUTH_CODE_FAILED",
+      description: "Failed to get access token using oauth code",
+    },
+  }),
+};
