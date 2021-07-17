cc1633eb8f535770910976e49a8376a2c7e59dbd
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 00:56:37 +0530

    caused by rename of src/index.ts -> src/app.ts

--- projects/yousound-api/src/controllers/AuthControllers.ts
+++ projects/yousound-api/src/controllers/AuthControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { googleOauth } from "..";
+import { googleOauth } from "../app";
 
 export const loginController = (_: Request, res: Response): void => {
   const googleOauthUrl = googleOauth.authUrl("offline");
