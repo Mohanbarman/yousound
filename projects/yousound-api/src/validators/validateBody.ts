aae81ed8eb746ee0a4f08fb5a5762d77948a6e18
mohanbarman <mohanbarman9711@gmail.com>
2021-07-18 01:04:54 +0530

    Adding validateBody function

---
+++ projects/yousound-api/src/validators/validateBody.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Request, NextFunction, Response } from "express";
+import { buildResponse } from "../utils/buildResponse";
+
+/**
+ * Responsible to validating request body
+ * @param name validator name, format : validators/fieldName
+ */
+export const validateBody =
+  (name: string) => async (req: Request, res: Response, next: NextFunction) => {
+    const [type, schema] = name.split("/");
+
+    const objectSchemas = await import(`./${type}/schemas.ts`);
+    const objectSchema = objectSchemas[schema];
+
+    try {
+      await objectSchema.validate(req.body);
+      next();
+    } catch (e) {
+      res.json(
+        buildResponse({
+          error: { code: "VALIDATION_FAILED", validationErrors: e },
+        })
+      );
+    }
+  };
