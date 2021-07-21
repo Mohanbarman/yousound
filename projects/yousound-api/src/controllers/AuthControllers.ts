7c8871175f7a0bb6cfcdd6f3d3478e9617f9d6ce
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:20:56 +0530

    Adding comment

--- projects/yousound-api/src/controllers/AuthControllers.ts
+++ projects/yousound-api/src/controllers/AuthControllers.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { googleOauth } from "../app";
 
+/**
+ * Redirects to google oauth consent screen
+ * @route GET /login
+ */
 export const loginController = (_: Request, res: Response): void => {
   const googleOauthUrl = googleOauth.authUrl("offline");
   res.redirect(googleOauthUrl);
