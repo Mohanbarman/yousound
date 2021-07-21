b73715fc9cdd510730eed75388d83dcd6cf90441
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:30:42 +0530

    Adding playlist, youtube-dl search errors

--- projects/yousound-api/src/errors/youtube.ts
+++ projects/yousound-api/src/errors/youtube.ts
@@ -10,4 +10,28 @@ export const youtubeErrors = {
       },
     });
   },
+
+  playlistNotFound: buildResponse({
+    success: false,
+    error: {
+      code: "PLAYLIST_NOT_FOUND",
+      description: "Playlist not found by id",
+    },
+  }),
+
+  youtubeDlFailed: buildResponse({
+    success: false,
+    error: {
+      code: "YOUTUBE_DL_FAILED",
+      description: "Youtub-dl failed to find the video",
+    },
+  }),
+
+  searchFailed: buildResponse({
+    success: false,
+    error: {
+      code: "SEARCH_FAILED",
+      description: "Failed to search for the video",
+    },
+  }),
 };
