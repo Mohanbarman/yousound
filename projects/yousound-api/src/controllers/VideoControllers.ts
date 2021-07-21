e0571bd87d77bc3eab6444ac9382a953f5103156
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:30:14 +0530

    Refactoring code

--- projects/yousound-api/src/controllers/VideoControllers.ts
+++ projects/yousound-api/src/controllers/VideoControllers.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import { getAudioUrl } from "@packages/youtube-dl";
 import { buildResponse } from "../utils";
+import { youtubeErrors } from "../errors";
 
+/**
+ * Get audio url of a youtube video using youtube-dl
+ * @route GET /video/:id
+ */
 export const getAudio = async (req: Request, res: Response): Promise<void> => {
   const videoId: string = req.params.id;
 
-  const { data: audioUrl, error: audioUrlErr } = await getAudioUrl(videoId);
+  // getting audio url using youtube-dl binary
+  const { data, error } = await getAudioUrl(videoId);
 
-  if (audioUrlErr) {
-    res.json({ error: audioUrlErr });
+  // failed to fetch audio url
+  if (error) {
+    res.json(youtubeErrors.youtubeDlFailed);
     return;
   }
 
-  res.json(buildResponse({ success: true, data: audioUrl }));
+  res.json(buildResponse({ success: true, data: { url: data } }));
 };
