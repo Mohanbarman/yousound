27009389df7a328e3037b55503ff66abd9ae43f8
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:18:56 +0530

    Adding endpoints in separate file

---
+++ packages/youtube-api/src/config/endpoints.ts
@@ -0,0 +1,5 @@
+export const endpoints = {
+  playlist: "https://www.googleapis.com/youtube/v3/playlists",
+  playlistItem: "https://www.googleapis.com/youtube/v3/playlistItems",
+  search: "https://www.googleapis.com/youtube/v3/search",
+};
