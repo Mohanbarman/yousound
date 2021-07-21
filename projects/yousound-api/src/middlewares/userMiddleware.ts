72818e38564de8b5b0c5f8ab6079ed05c9bb6803
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:31:14 +0530

    Refactoring code

--- projects/yousound-api/src/middlewares/userMiddleware.ts
+++ projects/yousound-api/src/middlewares/userMiddleware.ts
@@ -1,21 +1,34 @@
 import { Response, NextFunction } from "express";
+import { userErrors } from "../errors";
 import { TRequest } from "../types/context.types";
 import { userHelpers } from "../utils/userHelpers";
 
+/**
+ * Sets user in request object if user is authenticated
+ */
 export const getUser = async (
   req: TRequest,
-  _: Response,
+  res: Response,
   next: NextFunction
 ): Promise<any> => {
   const userId = parseInt(req.session.userID);
 
+  // no user id in session
   if (!userId) {
     return next();
   }
 
+  // getting user by user id
   const user = await userHelpers.getById(userId);
 
+  // user id not found in database
+  if (!user) {
+    return res.json(userErrors.notFound);
+  }
+
+  // setting user in user object
   req.user = user;
 
+  // calling next middleware
   next();
 };
