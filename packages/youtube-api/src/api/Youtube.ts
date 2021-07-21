dbc0f1fb7475e60da430c0a342a332c221d963cd
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:18:32 +0530

    adding searchQuery and getPlaylistItems method

--- packages/youtube-api/src/api/Youtube.ts
+++ packages/youtube-api/src/api/Youtube.ts
@@ -1,13 +1,12 @@
 import { IOauthConfig, IApiCallReturn } from "@packages/types";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { endpoints } from "../config/endpoints";
 import {
   IPlaylistsOptions,
   IPlaylistsResponse,
-} from "../types/playlists.types";
-
-const ENDPOINTS = {
-  PLAYLISTS: "https://www.googleapis.com/youtube/v3/playlists",
-};
+  IPlaylistItemsResponse,
+} from "../types";
+import { ISearchOptions, ISearchResponse } from "../types/search.types";
 
 /**
  * Responsible for interacting with google youtube api
@@ -19,12 +18,16 @@ export default class YoutubeApi {
     this.config = config;
   }
 
+  /**
+   * Get playlist of authenticated user
+   * @param accessToken
+   */
   async getMyPlaylists(
     accessToken: string,
     { part, ...options }: IPlaylistsOptions
   ): Promise<IApiCallReturn<IPlaylistsResponse>> {
     try {
-      const res = await axios.get(ENDPOINTS.PLAYLISTS, {
+      const res = await axios.get(endpoints.playlist, {
         params: { part: part.join(","), ...options },
         headers: { Authorization: `Bearer ${accessToken}` },
       });
@@ -34,6 +37,57 @@ export default class YoutubeApi {
         data: null,
         error: e?.response?.data?.error?.message || e.message,
       };
+    }
+  }
+
+  /**
+   * Get videos of playlist by playlist id
+   * @param playlistId
+   * @param accessToken
+   */
+  async getPlaylistItems(
+    playlistId: string,
+    accessToken: string,
+    { part, ...options }: IPlaylistsOptions
+  ): Promise<IApiCallReturn<IPlaylistItemsResponse>> {
+    try {
+      const params = {
+        params: { playlistId, part: part.join(","), ...options },
+        headers: { Authorization: `Bearer ${accessToken}` },
+      };
+
+      const res: AxiosResponse<IPlaylistItemsResponse> = await axios.get(
+        endpoints.playlistItem,
+        params
+      );
+      return { data: res.data, error: null };
+    } catch (e) {
+      return { data: null, error: "Playlist not found" };
+    }
+  }
+
+  /**
+   * Search for a video
+   * @param accessToken
+   */
+  async searchQuery(
+    accessToken: string,
+    { type, part, ...options }: ISearchOptions
+  ): Promise<IApiCallReturn<ISearchResponse>> {
+    try {
+      // search options
+      const params = {
+        type: type.join(","),
+        part: part.join(","),
+        ...options,
+      };
+      const res = await axios.get(endpoints.search, {
+        params,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      return { data: res.data, error: null };
+    } catch (e) {
+      return { data: null, error: "Failed to search video" };
     }
   }
 }
