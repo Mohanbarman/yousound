b1de3b6a78713d0c60edd2bb4ce258bbfcd518ca
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:32:07 +0530

    adding int field for validation

--- projects/yousound-api/src/validators/common/fields.ts
+++ projects/yousound-api/src/validators/common/fields.ts
@@ -1,4 +1,4 @@
 import * as yup from "yup";
 
-export const id = yup.number().required("This field is required");
+export const int = yup.number().required("This field is required");
 export const required = yup.string().required("This field is required");
