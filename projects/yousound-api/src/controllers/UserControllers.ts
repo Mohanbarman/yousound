399822efc159df74fa7d8538c1e400d5c615c33b
mohanbarman <mohanbarman9711@gmail.com>
2021-07-21 20:29:57 +0530

    Refactoring code

--- projects/yousound-api/src/controllers/UserControllers.ts
+++ projects/yousound-api/src/controllers/UserControllers.ts
@@ -1,15 +1,25 @@
 import { Response } from "express";
+import { userErrors } from "../errors";
 
 import { TRequest } from "../types/context.types";
+import { buildResponse } from "../utils";
 import { userHelpers } from "../utils/userHelpers";
 
-export const meController = async (
-  req: TRequest,
-  res: Response
-): Promise<void> => {
+/**
+ * Gets logged in user object from user's session
+ * @route GET /me
+ */
+export const getMe = async (req: TRequest, res: Response): Promise<number> => {
   const userId = parseInt(req.session.userID);
 
-  const dbUser = await userHelpers.getById(userId);
+  const user = await userHelpers.getById(userId);
 
-  res.send(dbUser);
+  // user not found
+  if (!user) {
+    res.json(userErrors.notFound);
+    return -1;
+  }
+
+  res.send(buildResponse({ success: true, data: user }));
+  return 0;
 };
